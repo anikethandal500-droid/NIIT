e0afd256db9ea95f2838f28e0c6613419771beeb
aniket <anikethandal500@gmail.com>
2026-02-24 22:19:56 +0530

    function

---
+++ for_loop.js
@@ -0,0 +1,8 @@
+let age = 21;
+
+if (age>=18){
+    console.log("you are eligible")
+}
+else{
+    console.log("you are not eligible")
+}
---
+++ function.js
@@ -0,0 +1,11 @@
+// function greet(name){
+//     console.log("welcome" + name)
+// }
+// greet (name)
+
+function age(yr){
+    if (yr<=18){
+        console.log("YOU CAN VOTE")
+    }
+}
+age(12)
